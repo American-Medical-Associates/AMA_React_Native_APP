979e280016641005784fab3e8e6769be85606621
Mckade Daines <mckadedaines1@gmail.com>
2023-08-29 10:46:45 -0700

    Error for empty input boxes have been implemented.

--- screens/IntMedMis.js
+++ screens/IntMedMis.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import InputBox from "../components/InputBox";
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View, ScrollView } from "react-native";
 import { DailyInternalMedMis } from "../firebase";
-import Picker from "../components/Picker";
+import MainButton from "../components/buttons/MainButton";
 
 //TODO:
 // - Connect to firebase and make sure it works
@@ -20,35 +20,77 @@ import Picker from "../components/Picker";
 // - Add a Dollars Collected input (required)
 // - Add a Submit Button (required)
 
-function IntMedMis() {
-  return (
-    <View style={styles.container}>
-      <InputBox
-        required={true}
-        style={styles.inputBox}
-        width={300}
-        placeholder={"Please Enter the Year"}
-      />
-      <InputBox
-        required={true}
-        style={styles.inputBox}
-        width={300}
-        placeholder={"Please Enter the Month"}
-      />
-      <InputBox
-        required={true}
-        style={styles.inputBox}
-        width={300}
-        placeholder={"Please Enter the Day"}
-      />
+const IntMedMis = () => {
+  const [year, setYear] = useState("");
+  const [requiredYear, setRequiredYear] = useState(false);
+  const [month, setMonth] = useState("");
+  const [requiredMonth, setRequiredMonth] = useState(false);
+  const [day, setDay] = useState("");
+  const [requiredDay, setRequiredDay] = useState(false);
+  const [provider, setProvider] = useState("");
+  const [requiredProvider, setRequiredProvider] = useState(false);
 
-      {/* THIS IS WHERE YOU LEFT OFF! */}
-      <Picker>
-        <Picker.Item label="Ashlee" value="Ashlee" />
-      </Picker>
-    </View>
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <InputBox
+          id={"year"}
+          required={true}
+          style={styles.inputBox}
+          width={300}
+          value={year}
+          onChangeText={(text) => setYear(text)}
+          placeholder={"Please Enter the Year"}
+        />
+        <InputBox
+          required={true}
+          style={styles.inputBox}
+          width={300}
+          value={month}
+          onChangeText={(text) => setMonth(text)}
+          placeholder={"Please Enter the Month"}
+        />
+        <InputBox
+          required={true}
+          style={styles.inputBox}
+          width={300}
+          value={day}
+          onChangeText={(text) => setDay(text)}
+          placeholder={"Please Enter the Day"}
+        />
+        <InputBox
+          required={true}
+          style={styles.inputBox}
+          width={300}
+          value={provider}
+          onChangeText={(text) => setProvider(text)}
+          placeholder={"Who was the Provider?"}
+        />
+        <MainButton
+          text={"Submit"}
+          buttonWidth={250}
+          onPress={() => {
+            if (year === "") {
+              alert("Please enter the current Year.");
+              setRequiredYear(true);
+            } else if (month === "") {
+              alert("Please enter the current Month.");
+              setRequiredMonth(true);
+            } else if (day === "") {
+              alert("Please enter the current Day.");
+              setRequiredDay(true);
+            } else if (provider === "") {
+              alert("Please enter the current Provider.");
+              setRequiredProvider(true);
+            } else {
+              alert("Works Correctly!");
+            }
+          }}
+        />
+      </View>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
